79889e3ef180bad8826f43b79d7c4fe9ba466537
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-03-06 14:38:42 -0800

    update app.js

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 import express from "express";
 import mariadb from 'mariadb';
 
-// const pool = mariadb.createPool({
-//     host: 
-//     user:
-//     password:
-//     database:
-//     port:
-// });
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+async function connect() {
+  try {
+      let conn = await pool.getConnection();
+      console.log("connected to database");
+      return conn;
+  } catch (err) {
+      console.log(`Error connecting to the database: ${err}`);
+  }
+}
 
 const app = express();
 
@@ -21,7 +31,7 @@ app.use(express.static("public"));
 // set the view engine for out application
 
 app.set('view engine', 'ejs');
-const PORT = 3000;
+const PORT = process.env.APP_PORT || 3000;
 
 app.get("/", (req, res) => {
   // Send our home page as a response to the client
