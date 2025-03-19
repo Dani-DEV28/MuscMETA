595f378c47888cc513dffd5e3541f210d13bcd4f
Nur Mohamed <mohamed.nur@student.greenriver.edu>
2025-03-19 10:58:28 -0700

    save

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ import mariadb from 'mariadb';
 import mysql from 'mysql2'
 import 'dotenv/config'; 
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    connectionLimit: 20
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 async function connect() {
