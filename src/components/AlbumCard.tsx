8e76b2f6a89926efc8f0b858a49897b063b45e38
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2026-06-14 10:01:39 -0700

    styling issues in the

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -9,7 +9,7 @@ export function AlbumCard({ album }: AlbumCardProps) {
     ? `data:${album.imageMime};base64,${Buffer.from(album.image).toString('base64')}`
     : '/img/CD-Image.jpg';
   return (
-    <Link href={`/album/${album.id}`} className="block w-full sm:w-[calc(50%-0.5rem)]">
+    <Link href={`/album/${album.id}`} className="block w-full]">
       <img src={src} alt={album.name} className="w-full aspect-square object-cover rounded" />
       <p className="text-center mt-1 text-text-muted">{album.name}</p>
     </Link>
